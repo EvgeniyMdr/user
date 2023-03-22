4241f0b738e783543fe581aad02ac604e10608cf
Evgeny <evgeniy.mudryy@inbox.ru>
2023-03-22 23:59:47 +0300

    added snippets

--- init.lua
+++ init.lua
@@ -2,6 +2,13 @@ return {
   colorscheme = "catppuccin",
   plugins = {
     {
+      "L3MON4D3/LuaSnip",
+      config = function(plugin, opts)
+        require "plugins.configs.luasnip"(plugin, opts)
+        require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } 
+      end,
+    },
+    {
       "catppuccin/nvim",
       as = "catppuccin",
       config = function()
